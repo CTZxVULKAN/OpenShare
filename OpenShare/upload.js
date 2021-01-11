e70c2050d9eaa93358221233da59ceb83314c240
BiswasJishnu <biswasjishnu24@gmail.com>
2021-01-11 20:22:14 +0530

    created a script to handle file input and upload.

---
+++ OpenShare/upload.js
@@ -0,0 +1,17 @@
+const fileInput = document.querySelector(".file_input");
+const fileUpload = document.querySelector(".uploadBtn");
+
+fileUpload.addEventListener("click", () => {
+  fileInput.click()
+});
+
+dropZone.addEventListener("drop", (e) => {
+  e.preventDefault()
+  const files = e.dataTransfer.files;
+  console.log(files)
+  if (files.length) {
+    fileInput.files;
+  }
+});
+
+
