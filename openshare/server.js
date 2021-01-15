e08f668893d79fe677c6677ce9b89119b567b131
BiswasJishnu <biswasjishnu24@gmail.com>
2021-01-15 21:00:54 +0530

    created an express server to serve the webpage.

--- openshare/server.js
+++ openshare/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}.`);
+const staticPath = path.join(__dirname);
+app.use(express.static(staticPath));
+
+
+app.get("/", (req, res) => {
 });
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}. Visit http://localhost:3000 to view.`);
+});
+
+
