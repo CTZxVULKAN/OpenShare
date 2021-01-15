0072ca21976c8bdf39c385ffdbe807b7a61d75de
BiswasJishnu <biswasjishnu24@gmail.com>
2021-01-15 20:58:22 +0530

    created to handle animations

---
+++ openshare/animation.js
@@ -0,0 +1,24 @@
+// This file handles all logic required for the animations.
+const dropZone = document.querySelector(".upload-section")
+
+dropZone.addEventListener("dragover", (e) => {
+  console.log("file dragged");
+  e.preventDefault()
+  if (!dropZone.classList.contains("dragged")) {
+    dropZone.classList.add("dragged")
+  }
+});
+
+dropZone.addEventListener("dragleave", () => {
+  console.log("file undragged")
+  dropZone.classList.remove("dragged")
+});
+
+dropZone.addEventListener("drop", (e) => {
+  e.preventDefault()
+  console.log("file dropped")
+  dropZone.classList.remove("dragged")
+});
+
+
+
