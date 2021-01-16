7c771c16431164fbe749fbe29f9e8e2f5bd81b25
BiswasJishnu <biswasjishnu24@gmail.com>
2021-01-16 09:51:03 +0530

    removed stuff that wasnt required.

--- openshare/server.js
+++ openshare/server.js
@@ -3,12 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const staticPath = path.join(__dirname);
-app.use(express.static(staticPath));
-
-
-app.get("/", (req, res) => {
-});
+const pagePath = path.join(__dirname);
+app.use(express.static(pagePath));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}. Visit http://localhost:3000 to view.`);
