40bf903be980193c75eeabc791f975ecced50c98
BiswasJishnu <biswasjishnu24@gmail.com>
2021-01-17 12:30:52 +0530

    implemented link generation on file upload via drag and drop.

--- openshare/upload.js
+++ openshare/upload.js
@@ -1,16 +1,35 @@
 // This file handles file input and upload.
 const fileInput = document.querySelector(".file_input");
 const fileUpload = document.querySelector(".uploadBtn");
+const host = "https://innshare.herokuapp.com";
+const uploadURL = `${host}/api/files`;
 
 fileUpload.addEventListener("click", () => {
-  fileInput.click()
+  fileInput.click();
 });
 
 dropZone.addEventListener("drop", (e) => {
-  e.preventDefault()
+  e.preventDefault();
   const files = e.dataTransfer.files;
-  console.log(files)
+  //console.log(files);
   if (files.length) {
-    fileInput.files;
+    fileInput.files = files;
+    uploadFile();
   }
 });
+
+const uploadFile = () => {
+  const file = fileInput.files[0];
+  const formData = new FormData();
+  formData.append("myfile", file);
+
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      console.log(xhr.response);
+    }
+  };
+
+  xhr.open("POST", uploadURL);
+  xhr.send(formData);
+};
