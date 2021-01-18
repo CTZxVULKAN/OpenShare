c487ea9d60b03db3e412ec48846d35d9f9d85f97
BiswasJishnu <biswasjishnu24@gmail.com>
2021-01-18 19:59:57 +0530

    added logic to the progressbar

--- openshare/upload.js
+++ openshare/upload.js
@@ -1,6 +1,7 @@
 // This file handles file input and upload.
 const fileInput = document.querySelector(".file_input");
 const fileUpload = document.querySelector(".uploadBtn");
+const bgProgress = document.querySelector(".bg-progress");
 const host = "https://innshare.herokuapp.com";
 const uploadURL = `${host}/api/files`;
 
@@ -34,6 +35,12 @@ const uploadFile = () => {
     }
   };
 
+  xhr.upload.onprogress = updateProgressbar;
   xhr.open("POST", uploadURL);
   xhr.send(formData);
+};
+
+const updateProgressbar = (e) => {
+  const progress = Math.round((e.loaded / e.total) * 100);
+  bgProgress.style.width = `${progress}%`
 };
