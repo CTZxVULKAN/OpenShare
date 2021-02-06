5319c46f79dbdbdfc0c5dabeeb912fc6af205e3d
BiswasJishnu <biswasjishnu24@gmail.com>
2021-02-06 09:19:44 +0530

    folder restructure

--- openshare/server.js
+++ openshare/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const pagePath = path.join(__dirname);
+const pagePath = path.join(__dirname, './frontend/');
 app.use(express.static(pagePath));
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}. Visit http://localhost:3000 to view.`);
+  console.log(`Server started on port ${PORT}. Visit http://localhost:${PORT} to view.`);
 });
 
 
