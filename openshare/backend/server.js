aaab1b4c129adbc9b964915d086a6a1c1582738c
BiswasJishnu <biswasjishnu24@gmail.com>
2021-02-24 16:22:39 +0530

    enabled server to render a download page

--- openshare/backend/server.js
+++ openshare/backend/server.js
@@ -4,14 +4,21 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const pagePath = path.join(__dirname, '../frontend/');
-app.use(express.static(pagePath));
+const frontendPath = path.join(__dirname, '../frontend/');
+app.use(express.static(frontendPath));
 
 const connectDB = require('./config/db');
 connectDB();
 
+// template engiene
+app.set('views', path.join(__dirname, '../backend/views/'));
+app.set('view engine', 'ejs');
+
+// route to handle post requests.
 app.use('/api/files', require('./routes/files'));
 
+// route to handle get requests.
+app.use('/files', require('./routes/render'));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}. Visit http://localhost:${PORT} to view.`);
